71f3eda5923ef373409dec975fb7f3b5463cc296
M Hasan <hasanmuhammad197@gmail.com>
2025-09-15 18:28:01 +0700

    Branch overview

---
+++ git-branching.js
@@ -0,0 +1,5 @@
+//Branch
+//Membuat timeline baru yang berbeda dari timeline utama (master/main)
+//Tidak ada batasan banyak branch di Git, biasanya dibuat ketika ingin menambah fitur baru
+//Fitur baru akan ditambah di branch baru, sehingga tidak takut ada kesalahan di branch utama
+//Ketika fitur baru sudah siap, bisa dilakukan merge (menggabungkan) branch baru ke branch utama
